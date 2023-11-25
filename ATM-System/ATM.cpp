51d33d1751ca471e75211fc516bddbdb900e85e7
DESKTOP-QR757PN\handohi <handohi9655@naver.com>
2023-11-25 22:52:44 +0900

    생성자 테스트

--- ATM-System/ATM.cpp
+++ ATM-System/ATM.cpp
@@ -15,3 +15,41 @@ void ATM::languageChange() {
 		this->language_setting = "English";
 	}
 }
+
+
+ATM::ATM(string input_primary_bank, int input_serial_number, string input_type, string input_lanuage_available, int* initial_fund[]) {
+	this->primary_bank = input_primary_bank;
+	this->serial_number = input_serial_number;
+	this->type = input_type;
+	this->language_available = input_lanuage_available;
+
+	for (int i = 0; i < 4; ++i) {
+		this->cash_storage[i] = initial_fund[i];
+	}
+}
+
+ATM::~ATM() {
+	this->primary_bank = "";
+	this->serial_number = 0;
+	this->type = "";
+	this->language_available = "";
+
+	for (int i = 0; i < 4; ++i) {
+		this->cash_storage[i] = 0;
+	}
+}
+
+
+
+void ATM::session() {
+
+	if (this->language_setting == "Korean") {
+
+	}
+	if (this->language_setting == "English") {
+
+	}
+
+}
+
+
--- ATM-System/ATM.h
+++ ATM-System/ATM.h
@@ -11,10 +11,20 @@ private:
 	int serial_number;
 	string admin_card;
 	string language_setting;
-	int* cash_storage[4]; // #1000, #5000, #10000, #50000
+	int* cash_storage[4]; // #1000, #5000, #10000, #50000 // initial fund
 	string transaction_histories;
+
+	string primary_bank;
+	string type;
+	string language_available;
+
 protected:
 public:
+
+	// primary bank name / serial numbeer / type : single or multi bank / language : uni, bi / initial fund
+	ATM(string input_primary_bank, int input_serial_number, string input_type, string input_lanuage_available, int* initial_fund[]);
+	~ATM();
+
 	void session();
 	void transaction();
 	void languageChange();
