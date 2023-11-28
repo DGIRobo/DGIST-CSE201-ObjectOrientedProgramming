24410ddf4bae5a4648b699bc92d0b066fb23a866
DGIRobo <48275742+DGIRobo@users.noreply.github.com>
2023-11-28 18:02:22 +0900

    preprocess to merge

--- ATM-System/SingleBankATM.h
+++ ATM-System/SingleBankATM.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "ATM.h"
-#include "Fee.h"
+//#include "Fee.h"
 
 using namespace std;
 
 class Single : public ATM {
 private:
-	Fee my_fee;
+	//Fee my_fee;
+	int fee_list[4];
 protected:
 public:
 	Single();
-	Single(string input_primary_bank, int input_serial_number, string input_type, string input_lanuage_available, int* initial_fund[]);
+	Single(string input_primary_bank, int input_serial_number, string input_type, string input_lanuage_available, int* initial_fund[], int fees[4]);
 	~Single();
 
-	void primary_deposit(Account a);
-	void primary_withdraw(Account a);
-	void primary_transfer(Account a, Account b);
-	void cash_transfer(Account a);
+	void deposit(Account a);
+	void withdraw(Account a);
+	void account_transfer(Account a, Account b);
+	void cash_transfer(Account b);
 };
