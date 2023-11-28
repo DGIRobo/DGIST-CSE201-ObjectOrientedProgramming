458fd7a8cc4afdbf9f2fe3706f7a6fa10c61ec5c
DGIRobo <48275742+DGIRobo@users.noreply.github.com>
2023-11-28 17:25:17 +0900

    Preprocessing to merge

--- ATM-System/Bank.h
+++ ATM-System/Bank.h
@@ -22,9 +22,9 @@ public:
 	int getBankId();
 	string getBankName();
 	void deposit2ATM(ATM* target_ATM, int numOf1000, int numOf5000, int numOf10000, int numOf50000);
-	
+
 	// Account* open_account(Account* a);
-	
+
 	void create_account();
 	Account* search_account_number();
 	Account* search_account_number_BankSearch(string input_account);
