1b4c2e2d33a8181c869a7d7afe48a793fcccf0f2
DGIRobo <48275742+DGIRobo@users.noreply.github.com>
2023-11-28 16:11:15 +0900

    main 함수 내의 Sevice Loop문 생성

--- ATM-System/main.cpp
+++ ATM-System/main.cpp
@@ -11,30 +11,6 @@
 using namespace std;
 
 /*
-Bank initialize_bank() {
-	cout << "Bank를 설립합니다." << endl << "Bank 이름을 입력해 주세요." << endl;
-	string name;
-	cin >> name;
-	return Bank(name);
-}
-
-void initialize_account(vector<Bank> list) {
-	cout << "초기 Account를 개설합니다." << endl;
-	cout << "계좌를 개설할 은행을 선택해 주세요." << endl;
-	for (int i = 0; i < list.size(); i++) {
-		cout << "[" << i << "] " << list.at(i).getBankName() << endl;
-	}
-	int bankselection;
-	cin >> bankselection;
-	if (sizeof(list) <= bankselection) {
-		list.at(bankselection).create_account();
-	}
-	else {
-		cout << "해당하는 은행을 찾을 수 없어 개설을 취소합니다." << endl;
-		return;
-	}
-}
-
 void initialize_Fee(int* fees) {
 	int fee_list[8];
 	cout << "Primary bank로부터의 deposit fee를 입력해 주십시오." << endl << "추천 Fee : 0" << endl;
@@ -119,30 +95,78 @@ ATM* initialize_ATM(vector<Bank> list) {
 }
 */
 
+int LanguageService(int language_setting) {
+	cout << "===========================<Language Change Session>===========================" << endl;
+	cout << "Select language" << endl;
+	cout << "1. English" << endl;
+	cout << "2. Korean" << endl;
+	cout << "Please Enter the Number: " << endl;
+	cin >> language_setting;
+	return language_setting;
+}
+
 int main() {
 	//initialization test
-	vector<Bank> bank_list;
-	vector<ATM> ATM_list;
-	vector<Account> account_list;
-	Bank Kakao("Kakao");
-	Kakao.create_account();
-	Kakao.makeCard_session();
-	/*
-	for (int i = 0; i != 0 ;) {
-		cout << "===bank initialization [" << i+1 << "]===" << endl;
-		bank_list.push_back(initialize_bank());
-		cout << "continue to press 0, stop to press 1(or else)" << endl;
-		cin >> i;
+	vector<Bank*> bank_list;
+	vector<ATM*> ATM_list;
+	vector<Account*> account_list;
+
+	bool onSession = true;
+	int language_setting = 1; // 1: English, 2: Korean
+
+	while (onSession) {
+		cout << "===========================<Welcome to Bank System Service>===========================" << endl;
+		if (language_setting == 1) {
+			cout << "Please Select the task you want to do." << endl;
+			cout << "1. Make Bank" << endl;
+			cout << "2. Banking Service (Make Account of Make Card)" << endl;
+			cout << "3. Make ATM" << endl;
+			cout << "4. ATM Service (Deposit, Withdraw, etc...)" << endl;
+			cout << "5. Change Language Setting" << endl;
+			cout << "6. Shut Down the Bank System Service" << endl;
+
+			cout << "Please Enter the Number: ";
+		}
+		if (language_setting == 2) {
+			cout << "진행하고자 하는 업무를 선택하세요." << endl;
+			cout << "1. 은행 만들기" << endl;
+			cout << "2. 은행 서비스 (계좌 및 카드 생성)" << endl;
+			cout << "3. ATM 만들기" << endl;
+			cout << "4. ATM 서비스 (입금, 출금 등...)" << endl;
+			cout << "5. 언어 설정 변경" << endl;
+			cout << "6. Bank System Service 종료" << endl;
+
+			cout << "숫자를 입력해주세요: ";
+		}
+
+		int session_chice;
+		cin >> session_chice;
+
+		switch (session_chice) {
+		case 1:
+			BankMake(bank_list);
+			break;
+		case 2:
+			BankService(bank_list);
+			break;
+		/*
+		case 3:
+			ATMMake(ATM_list);
+			break;
+		case 4:
+			ATMService(ATM_list);
+			break;
+		*/
+		case 5:
+			language_setting = LanguageService(language_setting);
+			break;
+		case 6:
+			onSession = false;
+			break;
+		}
+		cout << "===========================<End System Session>===========================" << endl;
 	}
-	for (int i = 0; i != 0;) {
-		cout << "===ATM initialization [" << i + 1 << "]===" << endl;
-		bank_list.push_back(initialize_bank());
-		cout << "continue to press 0, stop to press 1(or else)" << endl;
-		cin >> i;
-	}
-	cout << "======test======" << endl;
-	for (int i = 0; i < bank_list.size(); i++) {
-		cout << "Bank name : " << bank_list.at(i).getBankName() << endl;
-	}
-	*/
+
+	cout << "===========================<End System>===========================" << endl;
+	return 0;
 }
